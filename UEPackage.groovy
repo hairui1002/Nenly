b55dd6f3be5f354b136f065b446aff19ef3a22da
whr <2209615191@qq.com>
2022-02-18 11:01:31 +0800

    添加Pipeline测试脚本

--- UEPackage.groovy
+++ UEPackage.groovy
@@ -6,7 +6,7 @@ pipeline {
         stage('Package UE') {
       steps {
          bat '''md Z:\\虚幻内部版\\Nenly_4.27_79'''
-         bat '''xcopy C:\\Nenly_4.27_79 Z:\\虚幻内部版\Nenly_4.27_79'''
+         bat '''xcopy C:\\Nenly_4.27_79 Z:\\虚幻内部版\\Nenly_4.27_79'''
       }
         }
     }
