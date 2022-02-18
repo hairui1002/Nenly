db9189f019607a78c6977815234e706fcac43eaf
whr <2209615191@qq.com>
2022-02-18 10:57:59 +0800

    添加Pipeline测试脚本

---
+++ UEPackage.groovy
@@ -0,0 +1,13 @@
+pipeline {
+  agent any
+
+    stages {
+
+        stage('Package UE') {
+      steps {
+         bat '''md Z:\\虚幻内部版\Nenly_4.27_79'''
+         bat '''xcopy C:\\Nenly_4.27_79 Z:\\虚幻内部版\Nenly_4.27_79'''
+      }
+        }
+    }
+}
